1abe6d30b1065503c736b5d2bbf5e6b6802dd29f
Pedro Policello <88664809+PedroPolicello@users.noreply.github.com>
2023-11-07 22:27:53 -0300

    GameSystem Implemented

--- Checkpoint06 - GameProgramming/Assets/Scripts/Systems/GameSystem.cs
+++ Checkpoint06 - GameProgramming/Assets/Scripts/Systems/GameSystem.cs
@@ -5,7 +5,32 @@ using UnityEngine;
 public class GameSystem : MonoBehaviour
 {
     [SerializeField] private GameObject[] mobs;
+    [SerializeField] private GameObject[] thieves;
+    [SerializeField] private Vector2 minSpawnPosition, maxSpawnPosition;
 
+    private void Start()
+    {
+        SpawnMobs();
+        SpawnThieves();
+    }
 
+    private void SpawnMobs()
+    {
+        for (int i = 0; i < mobs.Length; i++)
+        {
+            float xPos = Random.Range(minSpawnPosition.x, maxSpawnPosition.x);
+            float yPos = Random.Range(minSpawnPosition.y, maxSpawnPosition.y);
+            Instantiate(mobs[i], new Vector2( xPos, yPos), Quaternion.identity);
+        }
+    }
 
+    private void SpawnThieves()
+    {
+        for (int i = 0; i < thieves.Length; i++)
+        {
+            float xPos = Random.Range(minSpawnPosition.x, maxSpawnPosition.x);
+            float yPos = Random.Range(minSpawnPosition.y, maxSpawnPosition.y);
+            Instantiate(thieves[i], new Vector2(xPos, yPos), Quaternion.identity);
+        }
+    }
 }
